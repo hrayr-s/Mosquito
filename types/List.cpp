8657e561f3195f2869ce8be5c77096594b88a1d3
hrayr-s <hro19969696@gmail.com>
2024-12-16 09:28:56 +0400

    Intermediate commit: changes from win laptop

---
+++ types/List.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hrayr on 30/08/2023.
+//
+
+#include "List.h"
---
+++ types/List.h
@@ -0,0 +1,16 @@
+//
+// Created by hrayr on 30/08/2023.
+//
+
+#ifndef DATABASE_LIST_H
+#define DATABASE_LIST_H
+
+
+
+class List {
+
+};
+
+
+
+#endif //DATABASE_LIST_H
